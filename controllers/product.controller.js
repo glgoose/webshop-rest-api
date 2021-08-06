4be18b77b90a58bdf91fb7a496e762ce6d3b97da
jacopobacci <bacci.jaco@gmail.com>
2021-08-06 16:28:14 +0200

    remove product controller

--- controllers/product.controller.js
+++
@@ -1,6 +0,0 @@
-const Product = require("../models/product.model");
-
-exports.getAllProducts = (req, res, next) => {
-  const products = await Product.find({}).sort("-createdAt").populate("category");
-  res.status(200).json({ message: "All product displayed correctly!", products });
-};
